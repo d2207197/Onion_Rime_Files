c2c7b91c1293502eb6693acf1994ad7aa7f38b43
oniondelta <deltazonetw@gmail.com>
2023-01-31 06:17:17 +0800

    再修整lua

--- allfiles/lua/filter_lua_custom_phrase_filter.lua
+++ allfiles/lua/filter_lua_custom_phrase_filter.lua
@@ -33,26 +33,32 @@ lua_custom_phrase_filter:
 ---------------------------------------------------------------
 
 local function load_text_dict(text_dict)
-  -- local slash = package.path:sub(1,1)  -- package.path 跑出的內容太長，不用
-  local path= rime_api.get_user_data_dir()
-  filename = path .. "/" .. ( text_dict or "lua_custom_phrase" ) .. ".txt" or ""  -- Mac 用
--- lua_filter 只會啟用 text_dict，如缺失無法自動轉 "lua_custom_phrase"，但 lua_translator 中重新部屬後即會轉換。
+  --- 當輸入 text_dict 不為 string 則跳開，該函數為 nil。
+  if type(text_dict) ~= "string" then return end
 
-  if io.open(filename) == nil then  -- Windows 用
-    filename = path .. "\\" .. ( text_dict or "lua_custom_phrase" ) .. ".txt" or ""
+  -- local slash = package.path:sub(1,1)  -- package.path 跑出的內容太長，不用。
+  local path= rime_api.get_user_data_dir()
+  filename = path .. "/" .. text_dict .. ".txt"  -- Mac 用
+  -- local filename = path .. "/" .. ( text_dict or "lua_custom_phrase" ) .. ".txt"  -- Mac 用（如 text_dict 為 nil，下方已跳開，可不用 or ）
+
+  if io.open(filename) == nil then
+    filename = path .. "\\" .. text_dict .. ".txt"  -- Windows 用
+    -- filename = path .. "\\" .. ( text_dict or "lua_custom_phrase" ) .. ".txt"  -- Windows 用（如 text_dict 為 nil，下方已跳開，可不用 or ）
   end
 
-  if io.open(filename) == nil then return {} end
+  --- 當找不到該 txt 字典檔案則跳開，該函數為 nil。
   -- if not isFile(filename) then return end  -- 在 widonws 中會有問題。
+  -- if io.open(filename) == nil then return log.error("lua_custom_phrase： Missing user_dict File!") end  -- 錯誤日誌中提示遺失檔案（不存在）
+  if io.open(filename) == nil then return end
 
   local tab = {}
   for line in io.open(filename):lines() do
     if not line:match("^#") then
-      if line:match("^[^\t]+\t[0-9a-z,./; -]+[\t]?%d*$") then
+      if line:match("^[^\t]+\t[%d%l,./; -]+\t?%d*$") then
 
         local line = string.gsub(line,"^([^\t]+\t[^\t]+)\t?.*$","%1")
-        local v_text = string.gsub(line,"^(.+)\t.+","%1")
-        local v_code = string.gsub(line,"^.+\t(.+)","%1")
+        local v_text = string.gsub(line,"^(.+)\t.+$","%1")
+        local v_code = string.gsub(line,"^.+\t(.+)$","%1")
         -- tab[v_code] = v_text  -- 一個 code 只能有一條短語，下方可一個 code，多條短語。
         if tab[v_code] == nil then
           local nn={}
@@ -71,10 +77,12 @@ end
 
 ---------------------------------------------------------------
 local function init(env)
-   config = env.engine.schema.config
-   namespace = "lua_custom_phrase_filter"
-   env.textdict = config:get_string(namespace .. "/user_dict") or ""
-   -- log.info("lua_custom_phrase_filter Initilized!")
+  config = env.engine.schema.config
+  namespace = "lua_custom_phrase_filter"
+  env.textdict = config:get_string(namespace .. "/user_dict") or ""
+  --- 以下 「load_text_dict」 可能為 nil 故要 or {}
+  env.tab = load_text_dict(env.textdict) or {}  -- 更新 txt 需「重新部署」或方案變換
+  -- log.info("lua_custom_phrase_filter: \'" .. env.textdict .. ".txt\' Initilized!")  -- 日誌中提示已經載入 txt 短語
 end
 
 
@@ -85,15 +93,20 @@ local function filter(input,env)
   local start = context:get_preedit().sel_start
   local _end = context:get_preedit().sel_end
   local caret_pos = context.caret_pos
-  local o_input = context.input  -- 原始未轉換輸入碼
-  -- local text_dict_tab = load_text_dict("lua_custom_phrase") or {}  -- 直接限定 txt 字典
-  local text_dict_tab = load_text_dict(env.textdict) or {}
   local es = _end - start
+  local o_input = context.input  -- 原始未轉換輸入碼
   local c_input = string.sub(o_input, -es)
-  local c_p_tab = text_dict_tab[c_input] or {}
-  -- local c_p_tab = text_dict_tab[o_input] or {}
+  --- 以下 「load_text_dict」 可能為 nil 故要 or {}
+  -- local text_dict_tab = load_text_dict("lua_custom_phrase") or {}  -- 直接限定 txt 字典
+  -- local text_dict_tab = load_text_dict(env.textdict) or {}  -- 更新 txt 不需「重新部署」
+  --- {}['xxx'] 拋出 nil
+  -- local c_p_tab = text_dict_tab[c_input]  -- 更新 txt 不需「重新部署」
+  -- local c_p_tab = text_dict_tab[o_input]  -- 更新 txt 不需「重新部署」
+  local c_p_tab = env.tab[c_input]
 
-  if text_dict_tab == {} then
+  if env.textdict == "" then
+  elseif env.tab == {} then
+  -- if text_dict_tab == {} then
   elseif c_p_tab then
   -- elseif caret_pos == #o_input and c_p_tab then
     for _, v in pairs(c_p_tab) do
--- allfiles/lua/translator_lua_custom_phrase.lua
+++ allfiles/lua/translator_lua_custom_phrase.lua
@@ -34,40 +34,43 @@ lua_custom_phrase:
 ---------------------------------------------------------------
 
 local function load_text_dict(text_dict)
+  --- 當輸入 text_dict 不為 string 則跳開，該函數為 nil。
+  if type(text_dict) ~= "string" then return end
+
   -- local slash = package.path:sub(1,1)  -- package.path 跑出的內容太長，不用。
   local path= rime_api.get_user_data_dir()
-  local filename = path .. "/" .. ( text_dict or "lua_custom_phrase" ) .. ".txt" or ""  -- Mac 用
+  filename = path .. "/" .. text_dict .. ".txt"  -- Mac 用
+  -- local filename = path .. "/" .. ( text_dict or "lua_custom_phrase" ) .. ".txt"  -- Mac 用（如 text_dict 為 nil，下方已跳開，可不用 or ）
 
-  if io.open(filename) == nil then  -- Windows 用
-    filename = path .. "\\" .. ( text_dict or "lua_custom_phrase" ) .. ".txt" or ""
+  if io.open(filename) == nil then
+    filename = path .. "\\" .. text_dict .. ".txt"  -- Windows 用
+    -- filename = path .. "\\" .. ( text_dict or "lua_custom_phrase" ) .. ".txt"  -- Windows 用（如 text_dict 為 nil，下方已跳開，可不用 or ）
   end
 
-  --- 當找不到該 txt 字典檔案則跳開（該函數為 nil）
+  --- 當找不到該 txt 字典檔案則跳開，該函數為 nil。
   -- if not isFile(filename) then return end  -- 在 widonws 中會有問題。
   -- if io.open(filename) == nil then return log.error("lua_custom_phrase： Missing user_dict File!") end  -- 錯誤日誌中提示遺失檔案（不存在）
   if io.open(filename) == nil then return end
 
   local tab = {}
   for line in io.open(filename):lines() do
-    local l_remove_sign = line:match("^#")
-    local l_ok = line:match("^[^\t]+\t[%d%l,./; -]+\t?%d*$")
+    if not line:match("^#") then
+      if line:match("^[^\t]+\t[%d%l,./; -]+\t?%d*$") then
 
-    if not l_remove_sign and l_ok then
-      local line = string.gsub(line,"^([^\t]+\t[^\t]+)\t?.*$","%1")
-      local v_text = string.gsub(line,"^(.+)\t.+$","%1")
-      local v_code = string.gsub(line,"^.+\t(.+)$","%1")
+        local line = string.gsub(line,"^([^\t]+\t[^\t]+)\t?.*$","%1")
+        local v_text = string.gsub(line,"^(.+)\t.+$","%1")
+        local v_code = string.gsub(line,"^.+\t(.+)$","%1")
+        -- tab[v_code] = v_text  -- 一個 code 只能有一條短語，下方可一個 code，多條短語。
+        if tab[v_code] == nil then
+          local nn={}
+          table.insert(nn, v_text)
+          tab[v_code] = nn
+        else
+          table.insert(tab[v_code], v_text)
+        end
 
-      -- tab[v_code] = v_text  -- 一個 code 只能有一條短語，下方可一個 code，多條短語。
-      if tab[v_code] == nil then
-        local nn={}
-        table.insert(nn, v_text)
-        tab[v_code] = nn
-      else
-        table.insert(tab[v_code], v_text)
       end
-
     end
-
   end
 
   return tab
@@ -78,6 +81,7 @@ local function init(env)
   config = env.engine.schema.config
   namespace = "lua_custom_phrase"
   env.textdict = config:get_string(namespace .. "/user_dict") or ""
+  --- 以下 「load_text_dict」 可能為 nil 故要 or {}
   env.tab = load_text_dict(env.textdict) or {}  -- 更新 txt 需「重新部署」或方案變換
   env.quality = 10
   -- log.info("lua_custom_phrase: \'" .. env.textdict .. ".txt\' Initilized!")  -- 日誌中提示已經載入 txt 短語
@@ -92,10 +96,12 @@ local function translate(input, seg, env)
   local engine = env.engine
   local context = engine.context
   -- local caret_pos = context.caret_pos
+  --- 以下 「load_text_dict」 可能為 nil 故要 or {}
   -- local text_dict_tab = load_text_dict("lua_custom_phrase") or {}  -- 直接限定 txt 字典
   -- local text_dict_tab = load_text_dict(env.textdict) or {}  -- 更新 txt 不需「重新部署」
-  -- local c_p_tab = text_dict_tab[input] or {}  -- 更新 txt 不需「重新部署」
-  local c_p_tab = env.tab[input] or {}  -- 更新 txt 需「重新部署」或方案變換
+  --- {}['xxx'] 拋出 nil
+  -- local c_p_tab = text_dict_tab[input]  -- 更新 txt 不需「重新部署」
+  local c_p_tab = env.tab[input]  -- 更新 txt 需「重新部署」或方案變換
 
   if c_p_tab then
   -- if caret_pos == #input and c_p_tab then  --只能在一開頭輸入，掛接後續無法。
