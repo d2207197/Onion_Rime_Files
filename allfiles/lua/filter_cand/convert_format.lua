e522f6c7c95a51c436c8cf58149679610d864e1e
oniondelta <deltazonetw@gmail.com>
2023-02-09 22:50:04 +0800

    修

--- allfiles/lua/filter_cand/convert_format.lua
+++ allfiles/lua/filter_cand/convert_format.lua
@@ -1,9 +1,11 @@
 --- 如同  comment_format
 
-local function convert_format(p1, ...) -- patern  支援utf8
+local function convert_format(...) -- patern  支援utf8
+-- local function convert_format(p1, ...) -- patern  支援utf8
   local config_list = ConfigList()
-  local format_set = {p1, ...}
-  for i ,v in next, format_set do
+  -- local format_set = {p1, ...}
+  -- for i ,v in next, format_set do
+  for i ,v in next, {...} do
     config_list:append(ConfigValue(v).element)
   end
   local proj = Projection()
