43f1f38de63e0ea5e30339284a89e77e8088d200
oniondelta <deltazonetw@gmail.com>
2023-04-08 22:24:57 +0800

    修

--- allfiles/lua/processor_kr_2set_0m_choice.lua
+++ allfiles/lua/processor_kr_2set_0m_choice.lua
@@ -8,7 +8,7 @@
 --]]
 
 
-local set_char = Set {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" , "Q", "W", "E", "R", "T" ,"O" ,"P"}  --> {a=true,b=true...}
+local set_char = Set {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "Q", "W", "E", "R", "T", "O", "P"}  --> {a=true,b=true...}
 -- local set_char = Set {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}  --> {a=true,b=true...}
 -- local set_number = Set {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
 
