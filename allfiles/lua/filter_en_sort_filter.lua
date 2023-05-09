022ad771e82d6a2cc64ebbcc9c49a99a535c0df1
oniondelta <deltazonetw@gmail.com>
2023-05-09 08:52:44 +0800

    目前不穩定，可能會出錯，且與說明不同，不用custom檔防崩控

--- allfiles/lua/filter_en_sort_filter.lua
+++ allfiles/lua/filter_en_sort_filter.lua
@@ -6,25 +6,25 @@
 
 ----------------------------------------------------------------------------------------
 
-local truncate_comment = require("filter_cand/truncate_comment")
+-- local truncate_comment = require("filter_cand/truncate_comment")
 
-local change_comment = require("filter_cand/change_comment")
+-- local change_comment = require("filter_cand/change_comment")
 
 local get_os_name = require("f_components/f_get_os_name")
 
--- local revise_comment_by_os = require("filter_cand/revise_comment_by_os")
+local revise_comment_by_os = require("filter_cand/revise_comment_by_os")
 
 ----------------------------------------------------------------------------------------
 
-local function revise_comment_by_os(os_name, cand, comment)
-  if os_name ~= 1 and os_name ~= 3 then
-  -- if os_name == 2 or os_name == 0 then
-  -- if os_name == 1 then  -- 測試用
-    local comment = string.gsub(truncate_comment(comment), "[\n%s]", "")  -- %s 為空白符
-    local cand = change_comment(cand, comment)
-  end
-  return cand
-end
+-- local function revise_comment_by_os(os_name, cand, comment)
+--   if os_name ~= 1 and os_name ~= 3 then
+--   -- if os_name == 2 or os_name == 0 then
+--   -- if os_name == 1 then  -- 測試用
+--     local comment = string.gsub(truncate_comment(comment), "[\n%s]", "")  -- %s 為空白符
+--     local cand = change_comment(cand, comment)
+--   end
+--   return cand
+-- end
 
 
 -- local M={}
@@ -65,18 +65,18 @@ local function filter(inp, env)
 
     if es < 3 then
 
-      for cand in inp:iter() do
-        cand.preedit = cand.preedit .. "\t（序排：二字母以下按個排）"  -- （序排：單字母按個排）
-        local cand = revise_comment_by_os(env.os_name, cand, cand.comment)
-        yield(cand)
-      end
-      -- yield(Candidate("en", start, _end, c_input, "〔小於〕"))  -- 測試用
-
-      -- local tran = Translation(revise_comment_by_os, env.os_name, inp) or inp
-      -- for cand in tran:iter() do
+      -- for cand in inp:iter() do
       --   cand.preedit = cand.preedit .. "\t（序排：二字母以下按個排）"  -- （序排：單字母按個排）
+      --   local cand = revise_comment_by_os(env.os_name, cand, cand.comment)
       --   yield(cand)
       -- end
+      -- -- yield(Candidate("en", start, _end, c_input, "〔小於〕"))  -- 測試用
+
+      local tran = Translation(revise_comment_by_os, env.os_name, inp) or inp
+      for cand in tran:iter() do
+        cand.preedit = cand.preedit .. "\t（序排：二字母以下按個排）"  -- （序排：單字母按個排）
+        yield(cand)
+      end
 
     else
 
@@ -102,30 +102,28 @@ local function filter(inp, env)
       if (#cands ~= 0) then  -- 按字母順序進行排序。
         table.sort(cands, function(a, b) return a.text == b.text and string.len(a.text) < string.len(b.text) or a.text < b.text end)
 
-        --- 以下用遍尋重組一個table，再排序，再新組 Candidate，還是太慢！
-        -- table.sort(cands, function(a, b) return a.text == b.text and a.index < b.index or a.text < b.text end)  --Rime是按編碼長度排序,所以要重排
+        -- --- 以下用遍尋重組一個table，再排序，再新組 Candidate，還是太慢！
+        -- -- table.sort(cands, function(a, b) return a.text == b.text and a.index < b.index or a.text < b.text end)  --Rime是按編碼長度排序,所以要重排
 
-        --- ipairs 比 pairs 更快，效能更好。但 ipairs 遇不連續數組會中斷。
-        --- pairs：迭代 table，可遍歷表中所有的 key 可以返回 nil。
-        --- ipairs: 迭代數組，不能返回 nil,如果遇到 nil 則退出。
-        for _, cand in ipairs(cands) do  -- 顯示
-          cand.preedit = cand.preedit .. "\t（序排：a~z）"  -- （序排：ａ～ｚ）
-          local cand = revise_comment_by_os(env.os_name, cand, cand.comment)
-          yield(cand)
-
-          --- 以下用遍尋重組一個精簡 table，再排序，再新組 Candidate，還是太慢！且 comment 附加尚需解決。
-          -- newcand = Candidate("en",0,caret_pos,cand.text," ")
-          -- yield(newcand)
-        end
-
-        -- --- 以下尚有問題未解決
-        -- -- local tran = cands:query()
-        -- local tran = Translation(revise_comment_by_os, env.os_name, cands)
-        -- -- for _, cand in ipairs(tran) do  -- 顯示
-        -- for cand in tran:iter() do
+        -- --- ipairs 比 pairs 更快，效能更好。但 ipairs 遇不連續數組會中斷。
+        -- --- pairs：迭代 table，可遍歷表中所有的 key 可以返回 nil。
+        -- --- ipairs: 迭代數組，不能返回 nil,如果遇到 nil 則退出。
+        -- for _, cand in ipairs(cands) do  -- 顯示
         --   cand.preedit = cand.preedit .. "\t（序排：a~z）"  -- （序排：ａ～ｚ）
+        --   local cand = revise_comment_by_os(env.os_name, cand, cand.comment)
         --   yield(cand)
+
+        --   --- 以下用遍尋重組一個精簡 table，再排序，再新組 Candidate，還是太慢！且 comment 附加尚需解決。
+        --   -- newcand = Candidate("en",0,caret_pos,cand.text," ")
+        --   -- yield(newcand)
         -- end
+
+        local tran = Translation(function() for _, cand in ipairs(cands) do yield(cand) end end)
+        local tran = Translation(revise_comment_by_os, env.os_name, tran) or tran
+        for cand in tran:iter() do
+          cand.preedit = cand.preedit .. "\t（序排：a~z）"  -- （序排：ａ～ｚ）
+          yield(cand)
+        end
 
         --- 以下防止記憶體洩漏暴漲？！不會立即清理記憶體，但會回退，測試！
         -- local cands = {}  -- 理論上不對，這邊不應該加local，但實際又有效果？觀察！
@@ -161,18 +159,18 @@ local function filter(inp, env)
 
   elseif not en_sort then
 
-    for cand in inp:iter() do
-      -- cand.preedit = cand.preedit .. "\t　　　開始：" .. start .. "結束：" .. _end  -- 測試位置用
-      cand.preedit = cand.preedit .. "\t（個排：字數）"  -- （個排：字母數）
-      local cand = revise_comment_by_os(env.os_name, cand, cand.comment)
-      yield(cand)
-    end
-
-    -- local tran = Translation(revise_comment_by_os, env.os_name, inp) or inp
-    -- for cand in tran:iter() do
+    -- for cand in inp:iter() do
+    --   -- cand.preedit = cand.preedit .. "\t　　　開始：" .. start .. "結束：" .. _end  -- 測試位置用
     --   cand.preedit = cand.preedit .. "\t（個排：字數）"  -- （個排：字母數）
+    --   local cand = revise_comment_by_os(env.os_name, cand, cand.comment)
     --   yield(cand)
     -- end
+
+    local tran = Translation(revise_comment_by_os, env.os_name, inp) or inp
+    for cand in tran:iter() do
+      cand.preedit = cand.preedit .. "\t（個排：字數）"  -- （個排：字母數）
+      yield(cand)
+    end
 
   end
 
