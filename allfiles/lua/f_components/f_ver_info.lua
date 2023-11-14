a51c64285feb5425e677a402df9cde9e18134f72
oniondelta <deltazonetw@gmail.com>
2023-11-15 00:05:17 +0800

    update check Version

--- allfiles/lua/f_components/f_ver_info.lua
+++ allfiles/lua/f_components/f_ver_info.lua
@@ -2,7 +2,12 @@
 檢查各種組件版本
 執行時記憶體會暴增
 --]]
-local function Version()
+local function Version(env)
+  if type(env) == "table" and env.engine then
+    if env.engine.context.composition:toSegmentation().get_segments then
+      return 215
+    end
+  end
   local ver
   if Opencc and Opencc('s2t.json').convert_word then
     return 200
@@ -33,20 +38,20 @@ local function Version()
   end
 end
 
-local function Ver_info()
-  if Version() >= 185 then
+local function Ver_info(env)
+  if Version(env) >= 185 then
     distribution_v = string.format("%s %s  (%s)",
     rime_api.get_distribution_code_name(),
     rime_api.get_distribution_version(),
     rime_api.get_distribution_name()) or ""
     librime_v = string.format("librime %s", rime_api.get_rime_version()) or ""
-    librime_lua_v = string.format("librime-lua %s", Version()) or ""
+    librime_lua_v = string.format("librime-lua %s", Version(env)) or ""
     lua_v = string.format("%s", _VERSION) or ""
     i_id = string.format("%s", rime_api.get_user_id()) or ""
-  elseif Version() >= 9 then
+  elseif Version(env) >= 9 then
     distribution_v = "librime-lua 小於 185，無判定函數"
     librime_v = string.format("librime %s", rime_api.get_rime_version()) or ""
-    librime_lua_v = string.format("librime-lua %s", Version()) or ""
+    librime_lua_v = string.format("librime-lua %s", Version(env)) or ""
     lua_v = string.format("%s", _VERSION) or ""
     i_id = "librime-lua 小於 185，無判定 id 函數"
   else
