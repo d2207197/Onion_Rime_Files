d0081872071973b517a362deea9cc71a69cd770a
oniondelta <deltazonetw@gmail.com>
2023-11-23 20:59:41 +0800

    revise

--- allfiles/lua/filter_mix_cf2_miss_filter.lua
+++ allfiles/lua/filter_mix_cf2_miss_filter.lua
@@ -105,9 +105,9 @@ local function filter(inp, env)
     end
   else
     for cand in inp:iter() do
-      -- local cand_text = cand.text
-      yield(cand.text == "。" and change_comment(cand,"〔句點〕") or
-            (cand.text == "〔" or cand.text == "〕") and change_comment(cand,"〔六角括號〕") or
+      local cand_text = cand.text
+      yield(cand_text == "。" and change_comment(cand,"〔句點〕") or
+            (cand_text == "〔" or cand_text == "〕") and change_comment(cand,"〔六角括號〕") or
             o_ascii_punct and c_input == "'" and Candidate("simp_apostrophe", 0, 1, "'", "") or
             cand )
     end
